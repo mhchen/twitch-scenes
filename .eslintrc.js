fb74fb28a77d2c785c5f15e863e4b6c8d9f5ab16
Mike Chen <1325549+mhchen@users.noreply.github.com>
2022-01-26 20:56:32 -0800

    Initial commit from Create Next App

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    "prettier/prettier": "error",
+    'react/react-in-jsx-scope': 0,
+  },
+}
