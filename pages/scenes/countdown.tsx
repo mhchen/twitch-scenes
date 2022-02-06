2af25792a3218cd73dde9fcdadc4c5c079be58fd
Mike Chen <1325549+mhchen@users.noreply.github.com>
2022-02-06 15:00:20 -0800

    Hook up countdown

--- pages/scenes/countdown.tsx
+++ pages/scenes/countdown.tsx
@@ -3,6 +3,7 @@ import type { ChannelProps } from '../../src/types';
 import getChannelServerSideProps from '../../src/getChannelServerSideProps';
 import Footer from '../../src/components/Footer';
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
 
 const Chat = dynamic(() => import('../../src/components/Chat'), { ssr: false });
 
@@ -12,12 +13,36 @@ export const getServerSideProps: GetServerSideProps<
   return getChannelServerSideProps();
 };
 
+const MINUTES = 4;
+const SECONDS = 50;
+
+const timerLength = 1000 * 60 * MINUTES + 1000 * SECONDS;
+
+const targetTime =
+  Math.floor((new Date().getTime() + timerLength) / 1000) * 1000;
+
 const Countdown: NextPage<ChannelProps> = ({ title }) => {
+  const [timeString, setTimeString] = useState('4:50');
+  useEffect(() => {
+    function tick() {
+      const timeRemaining = Math.max(0, targetTime - new Date().getTime());
+      const minutes = Math.floor(
+        (timeRemaining % (1000 * 60 * 60)) / (1000 * 60),
+      );
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+      setTimeString(`${minutes}:${seconds.toString().padStart(2, '0')}`);
+      const now = new Date().getTime();
+      const nextSecond = (Math.floor(now / 1000) + 1) * 1000;
+      setTimeout(tick, nextSecond - now);
+    }
+    tick();
+  }, []);
+
   return (
     <>
       <section className="source" data-grid-area="main">
         <p className="heading-1">Starting Soon!</p>
-        <p id="time">5:00</p>
+        <p className="time">{timeString}</p>
       </section>
       <section data-grid-area="host">
         <Chat />
