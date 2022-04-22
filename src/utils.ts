5c1cd7e91fceef43d2f756b416cedc9c54d7a053
Mike Chen <mhchen@gmail.com>
2022-04-21 23:11:17 -0700

    Update gist

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ interface Config {
 }
 
 const GIST_URL =
-  'https://gist.githubusercontent.com/mhchen/d57692c3a871ed6c2e6fe6342cd8d00a/raw/26b0e37330ec824c4bc3a5e9331db88fe0f18915/twitch-scene.json';
+  'https://gist.githubusercontent.com/mhchen/d57692c3a871ed6c2e6fe6342cd8d00a/raw/1112468935cdcb514e9563304ecf40ae4249245e/twitch-scene.json';
 
 export async function fetchConfig() {
   return fetch(GIST_URL).then((res) => res.json()) as Promise<Config>;
